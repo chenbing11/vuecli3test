f936bef10beb09d8f27400b11f50e2b73c8b309f
cb_json <cb@promote.cache-dns.local>
2020-07-13 23:10:03 +0800

    #00000

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import List from '../views/List.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* ç: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/list',
+    name: 'List',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* ç: "list" */ '../views/List.vue')
   }
 ]
 
---
+++ src/views/List.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="list">
+    <div class="header">
+       列表页面
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'List',
+  components: {
+  }
+}
+</script>
+<style lang="less" >
+  // @import "./../assets/style/varibles.less";
+  @import "@/assets/style/varibles.less";
+
+</style>
