20b1300eb1ccdfc642d7bd472771ad708e7cb7fe
cb_json <cb@promote.cache-dns.local>
2020-08-04 07:39:28 +0800

    #0000 loading

---
+++ src/utils/vuex-loading.js
@@ -0,0 +1,40 @@
+const NAMESPACED = 'myLoading';
+const createLoadingPlugin = (namespaced = NAMESPACED) => {
+  return store => {
+    if(store.state[namespaced]){
+      throw new Error ('createLoadingPlugin is exited');
+    }
+    store.registerModule(namespaced, {
+      namespaced:true,
+      state:{
+        dffect:{},
+      },
+      mutations:{
+        show(state,payload){
+          state.effect = {
+            ...state.effrct,
+            [payload]:true
+          }
+        },
+        hide(state,payload){
+          state.effect = {
+            ...state.effect,
+            [payload]:false
+          }
+        }
+      }
+    })
+
+    store.subscribeAction({
+      before:(action,state)=>{
+        console.log(`before action ${action.type}`);
+        store.commit(namespaced+"/show",action.type);
+      },
+      after:(action,state)=>{
+        console.log(`after action ${action.type}`);
+        store.commit(namespaced+'/hide',action.type);
+      }
+    })
+  }
+};
+export default createLoadingPlugin
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 //vue.config.js
+console.log(process.env.NODE_ENV);
+const port = process.env.port || 8888;
 const path = require('path');
-module.exports ={  
+function resolve (dir){
+    return path.join(__dirname,dir);
+}
+module.exports ={
     // configureWebpack: config => {
     //     if(process.env.NODE_ENV === 'production') {
     //         //为生产环境修改配置
@@ -8,10 +13,34 @@ module.exports ={
     //         //为开发环境修改配置
     //     }
     // }
+    lintOnSave:process.env.NODE_ENV === 'production',
+    devServer:{
+        port,
+        open:true,// 配置自动启动浏览器
+        proxy:{
+            [process.env.VUE_APP_BASE_API]:{
+                target:'http://localhost:8000',
+                changeORrigin:true,
+                pathReWrite:{
+                    [process.env.VUE_APP_BASE_API]: ''
+                }
+            }
+        }
+    },
     pluginOptions:{
-        'style-resources-loader': {
+        'style-resources-loader': { // less安装
             preProcessor: 'less',
             patterns: [path.resolve(__dirname,'./src/assets/style/varibles.lesss')]
         }
+    },
+    configureWebpack:{
+        resolve:{
+            alias:{
+                '@':resolve('src'),
+                'components':resolve('src/components'),
+                'utils':resolve('src/utils')
+            }
+        }
     }
+
 }
